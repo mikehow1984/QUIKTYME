9097b0f2f60e5c096b954d15e4eca141fdcd6772
mikehow1984 <mikehow1005@gmail.com>
2015-09-11 17:36:21 -0400

    finished making Fighter and Enemy separate classes

--- quik_funcs.h
+++ quik_funcs.h
@@ -6,30 +6,52 @@
 #include <stdlib.h>
 
 
-class Fighter //work in progress. Player stats
+class Fighter
+/*work in progress. Player stats. Thinking of making a system similar to
+* Pokemon, where you have 'base stats' that are common for that type of fighter,
+* 'individual values' that are static stats that are unchangeable when the
+* fighter is created, and 'effort values' which can be manipulated by the
+* the fights the fighter gets into. Pokemon also implements more elements into
+* its stat system, such as Natures (which raise and lower one stat by 10%
+* permanently), as well as items and moves that can temporarily raise stats.
+* I want to implement an equip-able item like a suit or costume which also
+* manipulates stats. Maybe a drop system like Diablo-type ARPG's with tons
+* of loot that you can find.
+*/
 {
 public:
     Fighter();
-    std::string name;
+    ~Fighter();
+
     int HP;
     int Atk;
     int Def;
     int Int;
     int Spd;
     double exp_gain;
+    std::string name;
 private:
     int Level;
     double exp;
 };
 
-class Enemy: public Fighter
+class Enemy
 {
+public:
+    Enemy();
+    ~Enemy();
+    std::string name;
+    int HP;
+    int Atk;
+    int Def;
+    int Int;
+    int Spd;
 
 };
 template <typename T> //randomize function for stats
-T stat_rand(T &stat)
-{
-    stat = rand()%10+1;
+T stat_rand(T &stat, int init_val, int range) //init_val = beginning range
+{                                             //range = max range
+    stat = rand()%range+init_val;
     return stat;
 }
 
