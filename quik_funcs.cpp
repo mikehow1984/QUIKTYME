3733cae43a1574bfdef862c47ed61b84523f839e
mikehow1984 <mikehow1005@gmail.com>
2015-09-10 21:32:19 -0400

    made Fighter and Enemy separate classes and added a constructor

--- quik_funcs.cpp
+++ quik_funcs.cpp
@@ -2,35 +2,35 @@
 #include <string>
 #include <stdlib.h>
 #include <cstdio>
+#include <ctime>
 #include "quik_funcs.h"
 
 using std::cin;
 using std::cout;
 
 
-/*
-int total_stat_rand (int &H, int &A, int &D, int &I, int &S)
-    {
-        H = stat_rand(H)+10;
-        A = stat_rand(A);
-        D = stat_rand(D);
-        I = stat_rand(I);
-        S = stat_rand(S);
-
-        return H;
-        return A;
-        return D;
-        return I;
-        return S;
-    }
-    */
+Fighter::Fighter()
+{
+    cout << "Enter your name: " << endl;
+    cin >> name;
+    Level = 1;
+    exp = 0;
+    srand(time(0));
+    HP = stat_rand(HP)+10;
+    Atk = stat_rand(Atk);
+    Def = stat_rand(Def);
+    Int = stat_rand(Int);
+    Spd = stat_rand(Spd);
+    exp = 0;
+    exp_gain = 0;
+}
 
 int input_errorchk (int n)
 {
         cin >> n;
         while(cin.fail() || (n != 0 && n != 1 && n != 2))
         {
-            printf("Invalid move! Re-enter: ");
+            cout << "Invalid move! Re-enter: ";
             cin.clear();
             cin.ignore(256,'\n');
             cin >> n;
@@ -40,11 +40,7 @@ int input_errorchk (int n)
 
 bool check_if_dead (int H)
 {
-    if (H <=0)
-    {
-        return true;
-    }
-    else{return false;}
+    return H <=0;
 }
 
 int move_power(int a, int d, int i, int h)
@@ -58,7 +54,7 @@ int move_power(int a, int d, int i, int h)
         crit = 2;
         cout << "CRITICAL HIT!\n";
     }
-    int power = ((a+h)-d+(rand()%4-2))*crit;
+    int power = ((a+h)-d+(rand()%4-2))*crit; //critical hit formula
     if (power < 0)
     {
         power = 0;
